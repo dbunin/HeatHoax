b2f963257bbbb8632463efef85f87379c5db7ad2
jose luis peña <jose.pena.te.winkel@semaku.com>
2017-12-15 11:36:12 +0100

    added login and regiister functionality

---
+++ front-end/src/app/components/admin/admin.component.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular.module('app')
+        .component('admin', {
+            templateUrl: 'app/components/admin/admin.tpl.html',
+            controller: HomeCtrl,
+            controllerAs: 'Admin',
+            replace: true
+        });
+
+    function HomeCtrl($scope, LeafletMap, Data, User) {
+        var vm = this;
+        vm.user = undefined;
+        vm.selectedRoute = 'login-register' //either register or login or login-register (default)
+
+        function init() {
+            vm.loggedIn = User.loggedIn();
+        }
+
+        vm.selectRoute = function (route) {
+            vm.selectedRoute = route;
+        };
+
+        vm.login = function (loginEmail, loginPassword) {
+            User.login(loginEmail, loginPassword).then(function (res) {
+                console.log(res);
+                if (res.result === 'Success') {
+                    console.log('success')
+                    vm.loggedIn = true;
+                    vm.user = User.get();
+                }
+            })
+        };
+
+        vm.register = function (registerEmail, registerPassword) {
+            User.register(registerEmail, registerPassword).then(function (res) {
+                if (res.result === 'Registered') {
+                    vm.loggedIn = true;
+                    vm.user = User.get();
+                }
+            })
+        };
+
+        init();
+    }
+})();
---
+++ front-end/src/app/services/user.factory.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+  
+    angular.module('app')
+      .factory('User', UserFactory);
+  
+    function UserFactory($http, $templateCache, config) {
+        var baseUrl = 'http://192.168.27.65:10001/'
+        var user = undefined;
+        return {
+            get: function () {
+                if (user) {
+                    return user;
+                }
+            },
+            loggedIn: function() {
+                return !_.isUndefined(user);
+            },
+
+            login: function(email, password) {
+                var resource = baseUrl + 'users/login/';
+                var params = {
+                    inputEmail: email,
+                    inputPassword: password
+                }
+                return $http({
+                    url: resource,
+                    method: "POST",
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: $.param(params)
+                }).then(function(res) {
+                    user = params;
+                    return res.data;
+                });
+            },
+            register: function(email, password) {
+                var resource = baseUrl + 'users/register/';
+                var params = {
+                    inputEmail: email,
+                    inputPassword: password
+                }
+                return $http({
+                    url: resource,
+                    method: "POST",
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    data: $.param(params)
+                }).then(function(res) {
+                    user = params;
+                    return res.data;
+                });
+            }
+        }
+    }
+  })();
